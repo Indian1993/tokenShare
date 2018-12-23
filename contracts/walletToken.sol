4625f040094c9dd9639a2f9011c65e3a623d3688
Anandhu K M <vivid.anandhu@gmail.com>
2018-12-23 16:08:43 +0530

    Add files via upload

---
+++ contracts/walletToken.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.5.0;
+
+contract walletToken {
+    string  public name = "Test Wallet";
+    string  public symbol = "WALL";
+    uint256 public totalSupply;
+    uint256 public tokenPrice = 1 ;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor (uint256 _initialSupply) public {
+    	//constructor to initialize amount of tokens
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+	//event for transaction of tokens b/w accounts
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
